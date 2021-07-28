2f333e51c1f0cf8d1190dc475c80b062a6f364bd
LeonardoRoFu <77032590+LeonardoRoFu@users.noreply.github.com>
2021-07-28 18:29:09 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,16 @@ void navegarAProductos() {
       .push(MaterialPageRoute(builder: (context) => Productos()));
 }
 
+void navegarAVentas() {
+  navigatorKey.currentState!
+      .push(MaterialPageRoute(builder: (context) => Ventas()));
+}
+
+void navegarAClientes() {
+  navigatorKey.currentState!
+      .push(MaterialPageRoute(builder: (context) => Productos()));
+}
+
 class Escritorio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -40,10 +50,11 @@ class Escritorio extends StatelessWidget {
                 ),
                 subtitle: Image(
                   image: AssetImage("C:\Users\yetla\StudioProjects\escritorio\Imagenes\productos.png")
-                )
                 ),
               ),
             ),
+              ),
+
         ],
       ),
     );
@@ -76,6 +87,68 @@ class Productos extends StatelessWidget {
           ),
           ),
         ]
+      ),
+    );
+  }
+}
+
+class Ventas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ventas"),
+      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              child: InkWell(
+                splashColor: Colors.green.withAlpha(30),
+                onTap: () {
+                  navegarAProductos();
+                },
+                child: ListTile(
+                  title: Text('Ventas',
+
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ]
+      ),
+    );
+  }
+}
+
+class Clientes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clientes"),
+      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              child: InkWell(
+                splashColor: Colors.red.withAlpha(30),
+                onTap: () {
+                  navegarAProductos();
+                },
+                child: ListTile(
+                  title: Text('Clientes',
+
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ]
       ),
     );
   }
